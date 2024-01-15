a4ee2c5f552f1eb9a0824c5c3d3a61c7a21b741d
JadeKim042386 <jk042386@gmail.com>
2024-01-15 11:11:23 +0900

    initialize multi module kotlin project

---
+++ docker/fintech-redis.sh
@@ -0,0 +1,5 @@
+docker network create redis-net
+docker run --name fintech-redis \
+             -p 6379:6379 \
+             --network redis-net \
+             -d redis:latest
---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+rootProject.name = "FinTech"
+include("api")
+include("consumer")
+include("gateway")
+include("css")
+include("domain")
