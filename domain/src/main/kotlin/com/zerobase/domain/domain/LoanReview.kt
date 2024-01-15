72715cd67e9c8e9937ae10c1853beb0d2c20a529
JadeKim042386 <jk042386@gmail.com>
2024-01-15 13:37:57 +0900

    feat: 도메인 엔티티와 레포지토리 추가

---
+++ domain/src/main/kotlin/com/zerobase/domain/domain/LoanReview.kt
@@ -0,0 +1,19 @@
+package com.zerobase.domain.domain
+
+import javax.persistence.*
+
+
+@Entity
+@Table(name = "LOAN_REVIEW")
+public class LoanReview(
+        @Column(name = "usr_key")
+        val userKey: String,
+        @Column(name = "loan_lmt_amt")
+        val loanLimitAmount: Long,
+        @Column(name = "loan_intrt")
+        val loanInterest: Double
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
+}
